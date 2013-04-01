5b0151eecb1f1d1206ff4656c93a53c2df3391f4
Patrick McFarland <diablod3@gmail.com>
2013-04-01 16:23:37 -0400

    Add .Xdefaults, tweak font sizes again

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 cd ~
+
 ln -s ~/config/vim/vimrc ~/.vimrc
 ln -s ~/config/vim ~/.vim
 mkdir ~/config/vim/undo
 mkdir ~/config/vim/tags
+
+ln -s ~/config/Xdefaults ~/.Xdefaults
+
 cd ~/config
 git submodule init && git submodule update
 
