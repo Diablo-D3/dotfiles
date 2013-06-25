ef72787a970d330a020872222e4a5b12637850ee
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2013-06-25 02:43:41 -0400

    Add dircolors-solarized

--- install.sh
+++ install.sh
@@ -6,14 +6,13 @@ ln -s ~/config/vim ~/.vim
 mkdir ~/config/vim/undo
 mkdir ~/config/vim/tags
 
-ln -s ~/config/Xdefaults ~/.Xdefaults
-
 ln -s ~/config/zsh/zshrc ~/.zshrc
 ln -s ~/config/zsh ~/.zsh
 
+ln -s ~/config/Xdefaults ~/.Xdefaults
 ln -s ~/config/tmux.conf ~/.tmux.conf
-
 ln -s ~/config/config/powerline ~/.config/powerline
+ln -s ~/config/dircolors-solarized/dircolors.ansi-dark ~/.dir_colors
 
 cd ~/config
 git submodule init && git submodule update
