2c9149d88c0ecee97bed9752aeadf75ba3bb6afe
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2013-12-05 10:11:54 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,9 +11,7 @@ ln -s ~/config/zsh ~/.zsh
 
 ln -s ~/config/Xresources ~/.Xresources
 ln -s ~/config/tmux.conf ~/.tmux.conf
-ln -s ~/config/config/powerline ~/.config/powerline
-ln -s ~/config/dircolors-solarized/dircolors.ansi-dark ~/.dir_colors
 
 cd ~/config
-git submodule init && git submodule update
+git submodule update --init --recursive
 
