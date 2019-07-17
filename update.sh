600d91fb3285a5c770332ebe23a9016b25e17c66
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2019-07-17 19:56:20 -0400

    I didn't forget an argument, I added too many

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git pull
-git submodule foreach git pull origin master
-git submodule foreach git checkout --theirs master
+git submodule foreach git pull
+git submodule foreach git checkout master
 git submodule foreach git reset --hard
 
