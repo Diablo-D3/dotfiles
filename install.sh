5f6b960c116d2cf4beb481446a1e3355e5719dbf
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2020-03-20 19:10:15 -0400

    Updated install script to add base16-mintty repo

--- install.sh
+++ install.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 cd ~
 
-ln -s ~/config/vim/vimrc ~/.vimrc
-ln -s ~/config/vim ~/.vim
-mkdir ~/config/vim/undo
-mkdir ~/config/vim/tags
+ln -sf ~/config/vim/vimrc ~/.vimrc
+ln -nsf ~/config/vim ~/.vim
+mkdir -p ~/config/vim/undo
+mkdir -p ~/config/vim/tags
 
 rm ~/.bashrc
 rm ~/.bash_profile
-ln -s ~/config/bashrc ~/.bashrc
-ln -s ~/config/bash_profile ~/.bash_profile
+ln -sf ~/config/bashrc ~/.bashrc
+ln -sf ~/config/bash_profile ~/.bash_profile
 
-ln -s ~/config/Xresources ~/.Xresources
-ln -s ~/config/tmux.conf ~/.tmux.conf
-ln -s ~/config/minttyrc ~/.minttyrc
-ln -s ~/config/toprc ~/.toprc
+ln -sf ~/config/Xresources ~/.Xresources
+ln -sf ~/config/tmux.conf ~/.tmux.conf
+ln -sf ~/config/minttyrc ~/.minttyrc
+ln -sf ~/config/toprc ~/.toprc
 
 cd ~/config
 git submodule update --init --recursive
 ./update.sh
+
+mkdir -p ~/.mintty
+ln -nsf ~/config/base16-mintty/mintty ~/.mintty/themes
 
