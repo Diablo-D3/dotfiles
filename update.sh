d09d976566d5d7339362b969dbbc587408b274f6
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2020-06-15 14:13:27 -0400

    Set submodule repos to ff only

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+git config pull.ff only
 git pull
+git submodule foreach git config pull.ff only
 git submodule foreach git pull
 git submodule foreach git checkout master
 git submodule foreach git reset --hard
