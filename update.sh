619c137f8e8a8fbb8580bb9eaea45b6e4fe49ae5
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2020-07-30 18:38:05 -0400

    Allow submodules to be virtually untracked

--- update.sh
+++ update.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-git config pull.ff only
-git pull
-git submodule foreach git config pull.ff only
-git submodule foreach git pull
-git submodule foreach git checkout master
-git submodule foreach git reset --hard
+git pull --recurse-submodules
+
 
