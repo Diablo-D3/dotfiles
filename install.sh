9a41e8017201e974c3dd2f3c3d435d36036d36ee
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2020-07-31 18:29:09 -0400

    Moved ~/config to ~/.dotfiles, make sure install.sh knows this

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 cd "$(dirname "$0")" || exit
 
-ln -sf ~/config/vim/vimrc ~/.vimrc
-ln -nsf ~/config/vim ~/.vim
+ln -sf ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -nsf ~/.dotfiles/vim ~/.vim
 mkdir -p ~/.vim/undo
 mkdir -p ~/.vim/tags
 
 rm ~/.bashrc
 rm ~/.bash_profile
-ln -sf ~/config/bashrc ~/.bashrc
-ln -sf ~/config/bash_profile ~/.bash_profile
+ln -sf ~/.dotfiles/bashrc ~/.bashrc
+ln -sf ~/.dotfiles/bash_profile ~/.bash_profile
 
-ln -sf ~/config/Xresources ~/.Xresources
-ln -sf ~/config/tmux.conf ~/.tmux.conf
-ln -sf ~/config/minttyrc ~/.minttyrc
-ln -sf ~/config/toprc ~/.toprc
-ln -sf ~/config/gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/Xresources ~/.Xresources
+ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/minttyrc ~/.minttyrc
+ln -sf ~/.dotfiles/toprc ~/.toprc
+ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
 
 mkdir -p ~/.mintty
-ln -nsf ~/config/base16-mintty/mintty ~/.mintty/themes
+ln -nsf ~/.dotfiles/base16-mintty/mintty ~/.mintty/themes
 
 git pull
