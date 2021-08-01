eea20deb63536dd9b92f38b87166ac59e5539b2d
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2021-08-01 14:48:51 -0400

    Fix typo

--- modules/vim.win.sh
+++ modules/vim.win.sh
@@ -3,4 +3,4 @@
 xln "${MODULE_DIR}/spacevim" "$USERPROFILE/vimfiles"
 xln "${MODULE_DIR}/spacevim" "$LOCALAPPDATA/nvim"
 xln "${MODULE_DIR}/spacevim.d" "$USERPROFILE/.SpaceVim"
-xln "${MODULE_DIR}/spacevim/d" "$USERPROFILE/.Spacevim.d"
+xln "${MODULE_DIR}/spacevim.d" "$USERPROFILE/.Spacevim.d"
