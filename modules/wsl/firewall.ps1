f93aab5d0288f67087d54c717735f5237800a5bf
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2021-09-06 09:25:06 -0400

    WSL2's port proxy isn't reliable, but neither is portproxy to WSL2's hyper-v switch; I'll stick with WSL2's custom method

--- modules/wsl/firewall.ps1
+++ modules/wsl/firewall.ps1
@@ -4,11 +4,9 @@ Remove-NetFireWallRule -DisplayName "WSL2"
 
 New-NetFireWallRule -DisplayName "WSL2" -Direction Inbound -LocalPort $ports -Action Allow -Protocol TCP
 
-$ip = wsl hostname -I
-
 netsh interface portproxy reset
 
 ForEach( $port in $ports ) {
-  netsh interface portproxy add v4tov4 listenport=$port connectaddress=$ip
+  netsh interface portproxy add v4tov4 listenport=$port connectaddress=127.0.0.1
 }
 
