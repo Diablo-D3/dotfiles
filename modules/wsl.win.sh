1d5a6844cf8ae12463e935c93f13836dd41aeeba
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2021-10-09 15:35:39 -0400

    include recursive support for ~/bin path, symlink windows ~/bin in ~/bin

--- modules/wsl.win.sh
+++ modules/wsl.win.sh
@@ -5,6 +5,9 @@ _ln "$USERPROFILE/Documents" "$HOME/Documents"
 _ln "$USERPROFILE/Downloads" "$HOME/Downloads"
 _ln "$USERPROFILE/workspace" "$HOME/workspace"
 
+_mkdir "$USERPROFILE/bin"
+_ln "$USERPROFILE/bin" "$HOME/bin/windows"
+
 _ln "$HOME/.cache" "$USERPROFILE/.cache"
 _ln "$HOME/.ssh" "$USERPROFILE/.ssh"
 _ln "$MODULE_DIR/windowsterminal.settings.json" "$LOCALAPPDATA/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
