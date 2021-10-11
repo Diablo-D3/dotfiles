0f7bd7ad1fb03f075c0961fae01254d7622471b5
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2021-10-11 07:34:04 -0400

    Add $host_ip, $vm_ip, and do not use them; forward to ::1 doesn't improve stability

--- modules/wsl/firewall.ps1
+++ modules/wsl/firewall.ps1
@@ -3,10 +3,15 @@ $ports = @(22);
 Remove-NetFireWallRule -DisplayName "WSL2"
 
 New-NetFireWallRule -DisplayName "WSL2" -Direction Inbound -LocalPort $ports -Action Allow -Protocol TCP
+New-NetFirewallRule -DisplayName "WSL2" -Direction Outbound -LocalPort $ports -Action Allow -Protocol TCP
+
+$host_ip = (Get-NetIPAddress -addressFamily IPv4 | where {($_.InterfaceAlias -NotMatch 'Loopback' -and $_.InterfaceAlias -NotMatch 'vEthernet' )} | select -First 1).IPAddress
+$vm_ip = bash.exe -c "hostname -I"
+
 
 netsh interface portproxy reset
 
 ForEach( $port in $ports ) {
-  netsh interface portproxy add v4tov6 listenport=$port connectaddress=::1
+  netsh interface portproxy add v4tov4 listenport=$port connectaddress=127.0.0.1
 }
 
