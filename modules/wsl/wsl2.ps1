d8f0be91d089e553802c7c97ff7c96c39d1ca149
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2021-12-09 12:42:42 -0500

    Possibly have finally fixed WSL2+portproxy issue, fingers crossed

--- modules/wsl/wsl2.ps1
+++ modules/wsl/wsl2.ps1
@@ -1,18 +1,31 @@
-wsl -u root service ssh start
+#
+# fake init.d
+#
+
+wsl -u root service ssh restart
+
+#
+# setup firewall
+#
 
 $ports = @(222);
 
 Remove-NetFireWallRule -DisplayName "WSL2"
 
 New-NetFireWallRule -DisplayName "WSL2" -Direction Inbound -LocalPort $ports -Action Allow -Protocol TCP
-New-NetFirewallRule -DisplayName "WSL2" -Direction Outbound -LocalPort $ports -Action Allow -Protocol TCP
 
-$host_ip = (Get-NetIPAddress -addressFamily IPv4 | where {($_.InterfaceAlias -NotMatch 'Loopback' -and $_.InterfaceAlias -NotMatch 'vEthernet' )} | select -First 1).IPAddress
-$vm_ip = bash.exe -c "hostname -I"
+$host_ip = (Get-NetIPAddress -addressFamily IPv4 -addressState Preferred | where {($_.InterfaceAlias -NotMatch 'Loopback' -and $_.InterfaceAlias -NotMatch 'vEthernet' )} | select -First 1).IPAddress
+$vm_ip = (Get-HnsEndpoint | where {$_.VirtualNetworkName -Eq 'WSL'} | Select-Object -First 1).IPAddress
 
 netsh interface portproxy reset
 
+# Connecting to VM's IP instead of WSL2's localhost proxy is more reliable
 ForEach( $port in $ports ) {
-  netsh interface portproxy add v4tov4 listenport=$port connectaddress=127.0.0.1
+  netsh interface portproxy add v4tov4 listenport=$port connectaddress=$vm_ip
 }
+
+netsh interface portproxy show all
+
+# Sometimes portproxy fails to work after reboot, this is not related to the WSL fast startup issue
+Restart-Service iphlpsvc
 
