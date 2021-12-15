e263f6dd9dd86ffea010ea29de7dcd06b160ec96
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2021-12-14 22:29:40 -0500

    Add xterm-24bits terminfo, override $TERM

--- modules/bash.any.sh
+++ modules/bash.any.sh
@@ -3,3 +3,5 @@
 _ln "$MODULE_DIR/bashrc" "$HOME/.bashrc"
 _ln "$MODULE_DIR/bash_profile" "$HOME/.bash_profile"
 
+_mkdir "$HOME/.terminfo/x"
+_ln "$MODULE_DIR/xterm-24bits" "$HOME/.terminfo/x/xterm-24bits"
