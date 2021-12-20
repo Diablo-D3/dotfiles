a8f69585709dd3a945c49dada4516977c256b77b
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2021-12-20 06:50:27 -0500

    Dont use xterm-24bits everywhere, rename to xterm-emacs

--- modules/bash.any.sh
+++ modules/bash.any.sh
@@ -4,4 +4,4 @@ _ln "$MODULE_DIR/bashrc" "$HOME/.bashrc"
 _ln "$MODULE_DIR/bash_profile" "$HOME/.bash_profile"
 
 _mkdir "$HOME/.terminfo/x"
-_ln "$MODULE_DIR/xterm-24bits" "$HOME/.terminfo/x/xterm-24bits"
+_ln "$MODULE_DIR/xterm-emacs" "$HOME/.terminfo/x/xterm-emacs"
