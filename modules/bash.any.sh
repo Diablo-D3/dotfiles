20c4760f99711b183cd93e3ef2ccc2eee2a6bdd7
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2022-01-06 01:04:01 -0500

    Add apt-clean, codify powerwashing Debian

--- modules/bash.any.sh
+++ modules/bash.any.sh
@@ -5,3 +5,7 @@ _ln "$MODULE_DIR/bash_profile" "$HOME/.bash_profile"
 
 _mkdir "$HOME/.terminfo/x"
 _ln "$MODULE_DIR/xterm-emacs" "$HOME/.terminfo/x/xterm-emacs"
+
+for bin in "$MODULE_DIR/bin/"*; do
+    _ln "$bin" "$HOME/bin/$(basename $bin)"
+done
