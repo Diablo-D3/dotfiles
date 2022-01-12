e02231d0837be4891982e7a9d515d664231b64a2
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2022-01-12 15:13:02 -0500

    Removed nvim config

--- modules/nvim.any.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-_ln "$MODULE_DIR/init.lua" "$HOME/.config/nvim/init.lua"
-_ln "$MODULE_DIR/site" "$HOME/.local/share/nvim/site"
-
--- modules/nvim.win.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-_ln "$MODULE_DIR/init.vim" "$LOCALAPPDATA/nvim/init.vim"
-_ln "$MODULE_DIR/site" "$LOCALAPPDATA/nvim/site"
-
