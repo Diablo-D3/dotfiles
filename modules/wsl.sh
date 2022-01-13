f4fcddc85a042a250c9b2c305d8a7637d582e005
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2022-01-13 14:52:37 -0500

    Clean up Windows support; don't scrub, force cp for ln, fix former actual cp

--- modules/wsl.sh
+++ modules/wsl.sh
@@ -12,6 +12,6 @@ if [ -v WSL ]; then
     _ln "$MODULE_DIR/windowsterminal.settings.json" "$LOCALAPPDATA/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
     _ln "$MODULE_DIR/windowsterminal.settings.json" "$LOCALAPPDATA/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState/settings.json"
 
-    _ln "$MODULE_DIR/hidden_powershell.js" "$USERPROFILE"
-    _ln "$MODULE_DIR/wsl2.ps1" "$USERPROFILE"
+    _ln "$MODULE_DIR/hidden_powershell.js" "$USERPROFILE/hidden_powershell.js"
+    _ln "$MODULE_DIR/wsl2.ps1" "$USERPROFILE/wsl2.ps1"
 fi
