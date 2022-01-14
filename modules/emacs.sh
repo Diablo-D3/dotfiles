590bfe1794ab5910a79ce8ee3aa70dd68b8bca1c
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2022-01-14 11:49:54 -0500

    Add Doom Emacs config

---
+++ modules/emacs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [ ! -d "$HOME/.emacs.d" ]; then
+    git clone --depth 1 "https://github.com/hlissner/doom-emacs" "$HOME/.emacs.d"
+    doom install
+    doom sync
+else
+    doom upgrade
+    doom sync
+fi
