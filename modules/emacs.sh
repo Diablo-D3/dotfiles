fbdccace948a2f279acc153e0c0384397d8991e9
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2022-01-19 01:37:10 -0500

    Call doom from canonical path during install

--- modules/emacs.sh
+++ modules/emacs.sh
@@ -2,8 +2,8 @@
 
 if [ ! -d "$HOME/.emacs.d" ]; then
     git clone --depth 1 "https://github.com/hlissner/doom-emacs" "$HOME/.emacs.d"
-    doom install -y
-    doom sync
+    "$HOME/.emacs.d/bin/doom" install -y
+    "$HOME/.emacs.d/bin/doom" sync
 else
     doom upgrade
     doom sync
