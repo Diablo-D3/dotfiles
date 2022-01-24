6ea939f0789d6694ba0d7fe1b40b53848b61b3c9
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2022-01-24 08:40:22 -0500

    Emacs: Install config twice on Win, once in WSL, once outside

--- modules/emacs.sh
+++ modules/emacs.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
-if [ ! -d "$HOME/.emacs.d" ]; then
-    git clone --depth 1 "https://github.com/hlissner/doom-emacs" "$HOME/.emacs.d"
-    "$HOME/.emacs.d/bin/doom" install -y
-    "$HOME/.emacs.d/bin/doom" sync
-else
-    doom upgrade
-    doom sync
+DOOM="$HOME/.emacs.d/bin/doom"
+
+declare -a DOOMDIRS
+DOOMDIRS=("$HOME/.emacs.d/")
+
+if [ ! -z ${WSL} ]; then
+    DOOMDIRS+=("$APPDATA/.emacs.d/")
 fi
+
+for DOOMDIR in "${DOOMDIRS[@]}"; do
+    if [ ! -d "$DOOMDIR" ]; then
+        git clone --depth 1 "https://github.com/hlissner/doom-emacs" "$DOOMDIR"
+
+        "$DOOM" -y install
+    else
+        "$DOOM" upgrade
+    fi
+done
