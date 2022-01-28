7a5e55a08a2d3963c43a5b7436250ee14bb8ccb6
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2022-01-28 06:08:01 -0500

    stealenv: Set both VAR and VAR, move _wslenv here

--- modules/wsl/stealenv.sh
+++ modules/wsl/stealenv.sh
@@ -6,11 +6,14 @@ vars=(APPDATA LOCALAPPDATA USERPROFILE)
   printf "#!/usr/bin/env bash\n\n";
 
   for var in "${vars[@]}"; do
-    _wslenv "$var";
-    printf "%s=%s\n" "$var" "${!var}";
+    varw=$(powershell.exe -Command "\$env:$var" | sed 's/\r//')
+    export "${var}W=$varw"
+    printf "%s=%s\n" "${var}W" "$varw"
+
+    varu=$(wslpath "$varw")
+    export "${var}=$varu"
+    printf "%s=%s\n" "$var" "$varu";
   done
 } > ~/.bashrc.local
 
 chmod +x ~/.bashrc.local
-
-
