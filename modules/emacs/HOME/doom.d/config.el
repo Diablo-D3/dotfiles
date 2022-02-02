8d6732414246b8f7159cbc224a6ce29b37869b01
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2022-02-01 21:41:11 -0500

    Set Iosevka Extended + Aile as emacs font

--- modules/emacs/HOME/doom.d/config.el
+++ modules/emacs/HOME/doom.d/config.el
@@ -19,8 +19,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "Iosevka Extended" :size 20)
+      doom-variable-pitch-font (font-spec :family "Iosevka Aile" :size 20))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -34,7 +34,6 @@
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
-
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
