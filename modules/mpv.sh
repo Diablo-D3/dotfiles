8fe2bcaa6d7d6580b27c25c489c2adc3dd9ff810
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2022-03-08 10:40:41 -0500

    Install mpv from deb-multimedia if mpv is installed

--- modules/mpv.sh
+++ modules/mpv.sh
@@ -3,3 +3,12 @@
 if [ -v WSL ]; then
    _ln_descent "$MODULE_HOME/config/mpv" "$APPDATA/mpv/"
 fi
+
+if [ -x "/usr/bin/mpv" ]; then
+   _sudo _ln_descent "$MODULE/apt" "/etc/apt/"
+fi
+
+if [ ! -f "/etc/apt/trusted.gpg.d/deb-multimedia-keyring.gpg" ]; then
+   _sudo apt update -oAcquire::AllowInsecureRepositories=true
+   _sudo apt install deb-multimedia-keyring
+fi
