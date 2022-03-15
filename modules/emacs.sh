a39e8b297c13543c0dfb28f5945ac6fb52451924
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2022-03-15 13:01:10 -0400

    emacs: fixed already installed detection

--- modules/emacs.sh
+++ modules/emacs.sh
@@ -4,12 +4,12 @@ EMACSD="$HOME/.emacs.d"
 DOOMD="$HOME/.doom.d"
 DOOM="$EMACSD/bin/doom"
 
-if [ ! -d "$HOME/${EMACSD}" ]; then
-    git clone --depth 1 "https://github.com/hlissner/doom-emacs" "${EMACSD}"
+if [ ! -d "$EMACSD" ]; then
+    git clone --depth 1 "https://github.com/hlissner/doom-emacs" "$EMACSD"
     "$DOOM" -y install --no-fonts
     "$DOOM" sync
 else
-    git -c "$EMACSD" pull --rebase
+    git -C "$EMACSD" pull --rebase
     "$DOOM" clean
     "$DOOM" sync -u -p
 fi
