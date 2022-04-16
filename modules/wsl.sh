5cfdebeb543fb9d105e04c54d972fb709b4376d7
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2022-04-16 16:49:58 -0400

    wsl: do not link ~/bin, but do link ~/.ssh

--- modules/wsl.sh
+++ modules/wsl.sh
@@ -5,14 +5,13 @@ if [ -v WSL ]; then
     _ln "$USERPROFILE/Documents" "$HOME/Documents"
     _ln "$USERPROFILE/Downloads" "$HOME/Downloads"
 
-    _mkdir "$USERPROFILE/bin"
-    _ln "$USERPROFILE/bin" "$HOME/bin/windows"
-
     _ln "$MODULE_DIR/windowsterminal.settings.json" "$LOCALAPPDATA/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
     _ln "$MODULE_DIR/windowsterminal.settings.json" "$LOCALAPPDATA/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState/settings.json"
 
     _ln "$MODULE_DIR/hidden_powershell.js" "$USERPROFILE/hidden_powershell.js"
     _ln "$MODULE_DIR/wsl2.ps1" "$USERPROFILE/wsl2.ps1"
+
+    _ln_descent "$HOME/.ssh" "$USERPROFILE/.ssh/"
 
     _scoop update
 fi
