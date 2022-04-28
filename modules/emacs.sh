57bfa1740b8b5167661051b54d76b9e746719ea6
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2022-04-28 19:20:04 -0400

    emacs: Install and keep up to date Iosevka

--- modules/emacs.sh
+++ modules/emacs.sh
@@ -43,4 +43,34 @@ if [ -n "${WSL+set}" ]; then
     fi
 fi
 
+# Install Iosevka
+wget -q "https://raw.githubusercontent.com/be5invis/iosevka/master/package.json" -O "/tmp/package.json"
+
+# Match '"version":', one or more whitespace, a double quote, one or more
+# whitespace, then flush the match; match one or more non-whitespace until you
+# reach one or more whitespace, a double quote, one or more whitespaces, and
+# then a comma.
+REGEX='"version":\s*"\s*\K\S+(?=\s*"\s*,)'
+
+I_VER="$(grep -oP "$REGEX" "/tmp/package.json")"
+I_URL="https://github.com/be5invis/Iosevka/releases/download/v$I_VER"
+
+I_VER_FILE="$HOME/.fonts/.iosevka.ver"
+
+_mkdir "$HOME/.fonts"
+
+if [ ! -f "$I_VER_FILE" ] || [ "$I_VER" != "$(<"$I_VER_FILE")" ]; then
+    _status "\nInstalling Iosevka $I_VER"
+
+    wget -q "$I_URL/super-ttc-iosevka-$I_VER.zip" -O "/tmp/iosevka.zip"
+    unzip -jo "/tmp/iosevka.zip" "iosevka.ttc" -d "$HOME/.fonts/"
+
+    wget -q "$I_URL/super-ttc-iosevka-aile-$I_VER.zip" -O "/tmp/iosevka-aile.zip"
+    unzip -jo "/tmp/iosevka-aile.zip" "iosevka-aile.ttc" -d "$HOME/.fonts/"
+
+    echo "$I_VER" >"$I_VER_FILE"
+else
+    _status "\nIosevka $I_VER already installed, skipping"
+fi
+
 exit 0
