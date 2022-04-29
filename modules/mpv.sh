8c2aeeb87343b7cbeecf9565065204a0ddc2e70b
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2022-04-29 04:07:10 -0400

    install: automatically install files from ETC

--- modules/mpv.sh
+++ modules/mpv.sh
@@ -4,10 +4,6 @@ if [ -n "${WSL+set}" ]; then
    _ln_descent "$MODULE_HOME/config/mpv" "$APPDATA/mpv/"
 fi
 
-if [ -x "/usr/bin/apt" ]; then
-   _sudo _ln_descent "$MODULE_DIR/etc/apt" "/etc/apt/"
-fi
-
 if [ ! -f "/etc/apt/trusted.gpg.d/deb-multimedia-keyring.gpg" ]; then
    _sudo apt update -oAcquire::AllowInsecureRepositories=true
    _sudo apt install deb-multimedia-keyring
--- modules/rustc.sh
+++ modules/rustc.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
 _mkdir ~/.cargo/bin
-curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.cargo/bin/rust-analyzer
+curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - >~/.cargo/bin/rust-analyzer
 chmod +x ~/.cargo/bin/rust-analyzer
-
-if [ -x "/usr/bin/apt" ]; then
-   _sudo _ln_descent "$MODULE_DIR/etc/apt" "/etc/apt/"
-fi
