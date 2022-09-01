5b692c163b846fb7e71a73c395486d163b06bf2d
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2022-09-01 19:40:50 -0400

    nvim: Force enable omnifunc, tagfunc, formatexpr until 0.8

--- modules/nvim/HOME/config/nvim/lua/config.lua
+++ modules/nvim/HOME/config/nvim/lua/config.lua
@@ -258,6 +258,11 @@ local lspfmt = vim.api.nvim_create_augroup('LspFormatting', {})
 
 local on_attach = function(client, bufnr)
     local bufopts = { noremap=true, silent=true, buffer=bufnr }
+
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    vim.api.nvim_buf_set_option(bufnr, 'tagfunc', 'v:lua.vim.lsp.tagfunc')
+    vim.api.nvim_buf_set_option(bufnr, 'formatexpr', 'v:lua.vim.lsp.formatexpr')
+
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
