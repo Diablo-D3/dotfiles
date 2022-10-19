605d7af32bead279465115177e5d94c04f07e62c
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2022-10-19 07:36:35 -0400

    hidden_powershell: Pass args to child ps1 script

--- modules/os-wsl/hidden_powershell.js
+++ modules/os-wsl/hidden_powershell.js
@@ -1,5 +1,11 @@
 var args = WScript.Arguments;
+var file = args(0);
+var fileargs = '';
 var wshShell = new ActiveXObject("WScript.Shell");
 
-wshShell.Run('%SystemRoot%\\system32\\WindowsPowerShell\\v1.0\\powershell.exe -ExecutionPolicy Bypass -File "' + args(0) + '"', 0, false);
+for (i = 1; i < args.length; i++) {
+    fileargs += (args(i) + " ");
+}
+
+wshShell.Run('%SystemRoot%\\system32\\WindowsPowerShell\\v1.0\\powershell.exe -ExecutionPolicy Bypass -File "' + file + '" ' + fileargs, 0, false);
 
