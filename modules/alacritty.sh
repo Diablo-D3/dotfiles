37f754b0944f8c5af08bf934708d96c906c7a7e4
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2023-01-22 16:21:38 -0500

    alacritty: no longer download/update Iosevka

--- modules/alacritty.sh
+++ modules/alacritty.sh
@@ -8,21 +8,21 @@ fonts="$HOME/.fonts"
 
 _mkdir "$fonts"
 
-_gh_dl "be5invis" "iosevka" "super-ttc-iosevka" "-VER" ".zip" "$fonts/iosevka.ttc"
-_gh_dl "be5invis" "iosevka" "super-ttc-iosevka-aile" "-VER" ".zip" "$fonts/iosevka-aile.ttc"
-
-if [ -f "/tmp/super-ttc-iosevka.zip" ]; then
-    unzip -qqjo "/tmp/super-ttc-iosevka.zip" "iosevka.ttc" -d "$fonts"
-    unzip -qqjo "/tmp/super-ttc-iosevka-aile.zip" "iosevka-aile.ttc" -d "$fonts"
-    rm -f "/tmp/super-ttc-iosevka.zip" "/tmp/super-ttc-iosevka-aile.zip"
-
-    if [ -n "${wsl+set}" ]; then
-        oldpwd="$PWD"
-        cd "$fonts" || exit
-        _ln "$modules_dir/os-wsl/fontreg.exe" "$USERPROFILE/bin/fontreg.exe"
-        powershell.exe -Command "& \$env:USERPROFILE\bin\fontreg.exe" "/copy"
-        cd "$oldpwd" || exit
-    fi
-fi
+# _gh_dl "be5invis" "iosevka" "super-ttc-iosevka" "-VER" ".zip" "$fonts/iosevka.ttc"
+# _gh_dl "be5invis" "iosevka" "super-ttc-iosevka-aile" "-VER" ".zip" "$fonts/iosevka-aile.ttc"
+#
+# if [ -f "/tmp/super-ttc-iosevka.zip" ]; then
+#     unzip -qqjo "/tmp/super-ttc-iosevka.zip" "iosevka.ttc" -d "$fonts"
+#     unzip -qqjo "/tmp/super-ttc-iosevka-aile.zip" "iosevka-aile.ttc" -d "$fonts"
+#     rm -f "/tmp/super-ttc-iosevka.zip" "/tmp/super-ttc-iosevka-aile.zip"
+#
+#     if [ -n "${wsl+set}" ]; then
+#         oldpwd="$PWD"
+#         cd "$fonts" || exit
+#         _ln "$modules_dir/os-wsl/fontreg.exe" "$USERPROFILE/bin/fontreg.exe"
+#         powershell.exe -Command "& \$env:USERPROFILE\bin\fontreg.exe" "/copy"
+#         cd "$oldpwd" || exit
+#     fi
+# fi
 
 exit 0
