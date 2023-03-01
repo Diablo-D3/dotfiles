705b72af393a918b3f1267b193ffece189bf5c14
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2023-03-01 16:16:42 -0500

    alacritty, bash: always compile alacritty.info, set TERM=alacritty

---
+++ modules/bash.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if _check_time "$HOME/.terminfo" "86400"; then
+    wget -q "https://github.com/alacritty/alacritty/blob/master/extra/alacritty.info" -o "/tmp/alacritty.info"
+    tic -x -o "$HOME/.terminfo" "/tmp/alacritty.info"
+fi
