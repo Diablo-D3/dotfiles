629d68d6aa3c7f00f49c9e2b89e0938b085ee469
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2023-03-06 19:47:11 -0500

    nvim: Move Neovim LuaLS lines to .luarc.json

--- modules/nvim/HOME/config/nvim/lua/config.lua
+++ modules/nvim/HOME/config/nvim/lua/config.lua
@@ -549,15 +549,6 @@ require('lspconfig').lua_ls.setup {
     on_attach = on_attach,
     settings = {
         Lua = {
-            runtime = {
-                version = 'LuaJIT',
-            },
-            diagnostics = {
-                globals = { 'vim' },
-            },
-            workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
-            },
             telemetry = {
                 enable = false,
             },
