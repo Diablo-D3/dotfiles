16e912ea016c8bf4060cbbb6e9d22bb8b84f238e
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2023-03-14 08:01:00 -0400

    nvim: Move stuff from .luarc.json back into config.lua

--- modules/nvim/HOME/config/nvim/lua/config.lua
+++ modules/nvim/HOME/config/nvim/lua/config.lua
@@ -535,6 +535,16 @@ require('lspconfig').lua_ls.setup {
     on_attach = on_attach,
     settings = {
         Lua = {
+            runtime = {
+                version = "LuaJIT"
+            },
+            diagnostics = {
+                globals = {'vim'}
+            },
+            workspace = {
+                checkThirdParty = false,
+                library = vim.api.nvim_get_runtime_file("", true)
+            },
             telemetry = {
                 enable = false,
             },
