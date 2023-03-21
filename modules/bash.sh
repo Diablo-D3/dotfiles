3cb90d4748d88a4627b4577b0f679e5711bdbb98
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2023-03-21 17:02:19 -0400

    alacritty, bash: use correct URL for alacritty terminfo

--- modules/bash.sh
+++ modules/bash.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 if _check_time "$HOME/.terminfo" "86400"; then
-    wget -q "https://github.com/alacritty/alacritty/blob/master/extra/alacritty.info" -O "/tmp/alacritty.info"
+    wget -q "https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info" -O "/tmp/alacritty.info"
     tic -x -o "$HOME/.terminfo" "/tmp/alacritty.info"
 fi
