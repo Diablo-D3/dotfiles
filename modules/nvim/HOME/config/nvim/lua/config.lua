7b5e013e4031de852dc8365466eb1ab7108b14c4
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2023-08-22 16:36:00 -0400

    nvim: rainbow delimiters default is good enough

--- modules/nvim/HOME/config/nvim/lua/config.lua
+++ modules/nvim/HOME/config/nvim/lua/config.lua
@@ -370,19 +370,6 @@ vim.cmd [[
     set foldexpr=nvim_treesitter#foldexpr()
 ]]
 
-local rainbow_delimiters = require('rainbow-delimiters')
-vim.g.rainbow_delimiters = {
-    strategy = {
-        [''] = rainbow_delimiters.strategy['global'],
-        vim = rainbow_delimiters.strategy['local'],
-    },
-    query = {
-        [''] = 'rainbow-delimiters',
-        lua = 'rainbow-blocks',
-    }
-}
-
-
 -----------
 -- mason --
 -----------
