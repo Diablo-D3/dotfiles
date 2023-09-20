469bdc8cd8c7b17c6ed363cd1aff32b43f2cfa8d
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2023-09-20 10:58:07 -0400

    nvim: Add mini.clue

--- modules/nvim/HOME/config/nvim/lua/config.lua
+++ modules/nvim/HOME/config/nvim/lua/config.lua
@@ -121,6 +121,53 @@ local mini_bufremove = require('mini.bufremove')
 mini_bufremove.setup({})
 vim.keymap.set('n', '<C-w>', function() mini_bufremove.wipeout(0, false) end, keyopts)
 
+-- mini.clue
+local mini_clue = require('mini.clue')
+mini_clue.setup({
+    triggers = {
+        -- Leader triggers
+        { mode = 'n', keys = '<Leader>' },
+        { mode = 'x', keys = '<Leader>' },
+
+
+        -- Built-in completion
+        { mode = 'i', keys = '<C-x>' },
+
+        -- `g` key
+        { mode = 'n', keys = 'g' },
+        { mode = 'x', keys = 'g' },
+
+        -- Marks
+        { mode = 'n', keys = "'" },
+        { mode = 'n', keys = '`' },
+        { mode = 'x', keys = "'" },
+        { mode = 'x', keys = '`' },
+
+        -- Registers
+        { mode = 'n', keys = '"' },
+        { mode = 'x', keys = '"' },
+        { mode = 'i', keys = '<C-r>' },
+        { mode = 'c', keys = '<C-r>' },
+
+        -- Window commands
+        { mode = 'n', keys = '<C-w>' },
+
+        -- `z` key
+        { mode = 'n', keys = 'z' },
+        { mode = 'x', keys = 'z' },
+    },
+
+    clues = {
+        -- Enhance this by adding descriptions for <Leader> mapping groups
+        mini_clue.gen_clues.builtin_completion(),
+        mini_clue.gen_clues.g(),
+        mini_clue.gen_clues.marks(),
+        mini_clue.gen_clues.registers(),
+        mini_clue.gen_clues.windows(),
+        mini_clue.gen_clues.z(),
+    },
+})
+
 -- mini.comment
 require('mini.comment').setup({})
 
@@ -660,3 +707,9 @@ vim.g.clipboard = {
 vim.cmd [[
     let g:tmux_navigator_preserve_zoom = 1
 ]]
+
+--------------
+-- epilogue --
+--------------
+
+mini_clue.ensure_buf_triggers()
