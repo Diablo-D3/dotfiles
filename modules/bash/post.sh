aca7c0a3b74f217cb416f8b85232fb14ee9bf3fd
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2023-12-12 15:03:53 -0500

    install: inflate _check_status into wrappers to remove eval, move input arg for time and ver to first arg to allow for multiple files per call

--- modules/bash/post.sh
+++ modules/bash/post.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if _check_time "$HOME/.terminfo" "86400"; then
-    wget -q "https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info" -O "/tmp/alacritty.info"
-    tic -x -o "$HOME/.terminfo" "/tmp/alacritty.info"
+if _check_time "86400" "$HOME/.terminfo"; then
+	wget -q "https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info" -O "/tmp/alacritty.info"
+	tic -x -o "$HOME/.terminfo" "/tmp/alacritty.info"
 fi
