c54754d41f063d335d61bd0e2cde676130258b08
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2023-12-20 05:24:41 -0500

    alacritty: Iosevka changed package names, change to downloading SuperTTC files; also install script escaped hardening pass

--- modules/alacritty/install.sh
+++ modules/alacritty/install.sh
@@ -1,30 +1,34 @@
 #!/usr/bin/env bash
 
-if [ -n "${WSL+set}" ]; then
-    _stow "$MODULE_HOME/config/alacritty" "$APPDATA/alacritty/"
+# shellcheck enable=all
+# shellcheck source=install-lib
+source "${BASE_DIR:?}/install-lib"
+
+if [[ -n "${WSL+set}" ]]; then
+	_stow "${MODULE_HOME:?}/config/alacritty" "${APPDATA:?}/alacritty/"
 fi
 
-fonts="$HOME/.fonts"
-term="ttf-iosevka-term"
-aile="ttf-iosevka-aile"
+fonts="${HOME}/.fonts"
+iosevka="SuperTTC-Iosevka"
+aile="SuperTTC-IosevkaAile"
 
-_mkdir "$fonts"
+_mkdir "${fonts}"
 
-_gh_dl "be5invis" "iosevka" "$term" "-VER" ".zip" "$fonts/$term-regular.ttf"
-_gh_dl "be5invis" "iosevka" "$aile" "-VER" ".zip" "$fonts/$aile-regular.ttf"
+_gh_dl "be5invis" "iosevka" "${iosevka}" "-VER" ".zip" "${fonts}/Iosevka.ttc"
+_gh_dl "be5invis" "iosevka" "${aile}" "-VER" ".zip" "${fonts}/IosevkaAile.ttc"
 
-if [ -f "/tmp/$term.zip" ] || [ -f "/tmp/$aile" ]; then
-    unzip -qqjo "/tmp/ttf-iosevka-term.zip" "*ttf" -d "$fonts"
-    unzip -qqjo "/tmp/ttf-iosevka-aile.zip" "*ttf" -d "$fonts"
-    rm -f "/tmp/$term.zip" "/tmp/$aile.zip"
+if [[ -f "/tmp/${iosevka}.zip" ]] || [[ -f "/tmp/${aile}.zip" ]]; then
+	unzip -qqjo "/tmp/${iosevka}.zip" "*ttc" -d "${fonts}"
+	unzip -qqjo "/tmp/${aile}.zip" "*ttc" -d "${fonts}"
+	rm -f "/tmp/${iosevka}.zip" "/tmp/${aile}.zip"
 
-    if [ -n "${WSL+set}" ]; then
-        oldpwd="$PWD"
-        cd "$fonts" || exit
-        _ln "$MODULES_DIR/os-wsl/fontreg.exe" "$USERPROFILE/bin/fontreg.exe"
-        powershell.exe -Command "& \$env:USERPROFILE\bin\fontreg.exe" "/copy"
-        cd "$oldpwd" || exit
-    fi
+	if [[ -n "${WSL+set}" ]]; then
+		oldpwd="${PWD}"
+		cd "${fonts}" || exit
+		_ln "${MODULES_DIR:?}/os-wsl/fontreg.exe" "${USERPROFILE:?}/bin/fontreg.exe"
+		powershell.exe -Command "& \$env:USERPROFILE\bin\fontreg.exe" "/copy"
+		cd "${oldpwd}" || exit
+	fi
 fi
 
 exit 0
