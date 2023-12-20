6f667a99fe91d62218652738eea1e6acc60769d8
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2023-12-20 05:22:30 -0500

    install: Adapt calls of check functions to check magic global return

--- modules/bash/install.sh
+++ modules/bash/install.sh
@@ -4,8 +4,8 @@
 # shellcheck source=install-lib
 source "${BASE_DIR:?}/install-lib"
 
-check_time=$(_check_time "86400" "${HOME}/.terminfo")
-if ${check_time}; then
+_check_time "86400" "${HOME}/.terminfo"
+if [[ "${__check_time}" -eq 0 ]]; then
 	wget -q "https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info" -O "/tmp/alacritty.info"
 	tic -x -o "${HOME}/.terminfo" "/tmp/alacritty.info"
 fi
