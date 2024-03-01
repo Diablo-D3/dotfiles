1fd105cefbfc8791a0a4419bf3a82fb0f02d8b01
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2024-03-01 16:18:45 -0500

    wezterm: Add support

---
+++ modules/wezterm/home/wezterm.lua
@@ -0,0 +1,56 @@
+local wezterm = require('wezterm')
+local config = wezterm.config_builder()
+
+config.enable_tab_bar = false
+
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0
+}
+
+config.font = wezterm.font('Iosevka Term', { weight = 'Regular' })
+
+config.font_rules = {
+    {
+        intensity = 'Bold',
+        italic = false,
+        font = wezterm.font('Iosevka Term', { weight = 'Medium' })
+    },
+    {
+        intensity = 'Bold',
+        italic = true,
+        font = wezterm.font('Iosevka Term', { weight = 'Medium', italic = true })
+    },
+}
+
+config.font_size = 14.0
+
+config.keys = {
+    {
+        key = 'F11',
+        mods = '',
+        action = wezterm.action.ToggleFullScreen
+    }
+}
+
+config.default_prog = { 'bash' }
+
+config.initial_cols = 80
+config.initial_rows = 25
+
+wezterm.on('window-config-reloaded', function(_, pane)
+    local size = pane:get_dimensions()
+    local cell_width = math.floor(size.pixel_width / size.cols + 0.5)
+    local cell_height = math.floor(size.pixel_height / size.viewport_rows + 0.5)
+
+    local term_width = math.floor(size.cols * cell_width + 0.5)
+    local term_height = math.floor(size.viewport_rows * cell_height + 0.5)
+
+    wezterm.log_info('cell size: ' .. cell_width .. 'x' .. cell_height)
+    wezterm.log_info('term size: ' .. term_width .. 'x' .. term_height)
+    wezterm.log_info('tput size: ' .. size.cols .. 'x' .. size.viewport_rows)
+end)
+
+return config
