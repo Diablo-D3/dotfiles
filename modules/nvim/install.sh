93f87727b0790fe2ffe84cc4c2aaf7188c4011fa
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2024-03-04 11:12:29 -0500

    nvim: Download nvim appimage to tmp file then mv

--- modules/nvim/install.sh
+++ modules/nvim/install.sh
@@ -7,6 +7,7 @@ source "${BASE_DIR:?}/install-lib"
 _check_time "86400" "${HOME}/.bin/nvim"
 
 if [[ "${__check_time}" -eq 0 ]]; then
-	wget -O "${HOME}/.bin/nvim" "https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage"
-	chmod u+x "${HOME}/.bin/nvim"
+	wget -O "${HOME}/.bin/.nvim.new" "https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage"
+	chmod u+x "${HOME}/.bin/.nvim.new"
+	mv "${HOME}/.bin/.nvim.new" "${HOME}/.bin/nvim"
 fi
