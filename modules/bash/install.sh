bbc34dc0ebff1185ada289e2464ed60771fabe59
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2024-03-19 12:41:48 -0400

    bash: Add check for wezterm terminfo, remove timer

--- modules/bash/install.sh
+++ modules/bash/install.sh
@@ -4,10 +4,16 @@
 # shellcheck source=install-lib
 source "${BASE_DIR:?}/install-lib"
 
-_check_time "86400" "${HOME}/.terminfo"
-if [[ "${__check_time}" -eq 0 ]]; then
+if [[ ! -f "${HOME}/.terminfo/a/alacritty" ]]; then
 	wget -q "https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info" -O "/tmp/alacritty.info"
 	tic -x -o "${HOME}/.terminfo" "/tmp/alacritty.info"
 
 	rm -f "/tmp/alacritty.info"
+fi
+
+if [[ ! -f "${HOME}/.terminfo/w/wezterm" ]]; then
+	wget -q "https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo" -O "/tmp/wezterm.info"
+	tic -x -o "${HOME}/.terminfo" "/tmp/wezterm.info"
+
+	rm -f "/tmp/wezterm.info"
 fi
