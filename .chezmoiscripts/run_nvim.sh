97906774ef226927aa62a83557847e149c391075
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2025-01-29 23:19:42 -0500

    nvim: Update release filename

--- .chezmoiscripts/run_nvim.sh
+++ .chezmoiscripts/run_nvim.sh
@@ -24,7 +24,7 @@ if (command -v "nvim" >/dev/null 2>&1); then
     fi
 
     if [ "${check}" -eq 0 ]; then
-        _gh_dl "neovim" "neovim" "nvim.appimage" "browser_download_url" "/tmp/nvim"
+        _gh_dl "neovim" "neovim" "nvim-linux-x86_64.appimage" "browser_download_url" "/tmp/nvim"
         if [ -f "/tmp/nvim" ]; then
             chmod u+x "/tmp/nvim"
             mv "/tmp/nvim" "${HOME}/.local/bin/nvim"
