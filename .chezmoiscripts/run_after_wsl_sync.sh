f18ec422d9a1ba597790e3abda1e626ee76f0094
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2025-07-28 18:37:25 -0400

    chezmoi/wsl: Remove now duplicate Kanata sync code

--- .chezmoiscripts/run_after_wsl_sync.sh
+++ .chezmoiscripts/run_after_wsl_sync.sh
@@ -3,12 +3,12 @@
 # shellcheck source=.chezmoitemplates/install-lib
 . "${HOME}/.local/share/chezmoi/.chezmoitemplates/install-lib"
 
-_msg "Running wsl (after)"
-
 case "${CHEZMOI_OS:?}" in
 "linux")
     case "${CHEZMOI_KERNEL_OSRELEASE:?}" in
     *"microsoft"*)
+        _msg "Running wsl (after)"
+
         if [ -f "${HOME}/.bashrc.win" ]; then
             . "${HOME}/.bashrc.win"
         fi
@@ -33,35 +33,6 @@ case "${CHEZMOI_OS:?}" in
         mkdir -p "${LOCALAPPDATA:?}/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/"
         cp "${SRC:?}/src/wsl/windowsterminal.settings.json" "${LOCALAPPDATA:?}/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState/settings.json"
 
-        # kanata
-        mkdir -p "${USERPROFILE:?}/kanata"
-        cp "${SRC:?}/src/kanata/kanata.ps1" "${USERPROFILE:?}/kanata/"
-        cp "${SRC:?}/src/kanata/"*".kbd" "${USERPROFILE:?}/kanata/"
-
-        new=$(date +%s)
-        state="${HOME}/.config/chezmoi/run_after_wsl_sync_kanata.time"
-
-        check=1
-
-        if [ ! -e "${state}" ]; then
-            printf "%s" "${new}" >"${state}"
-            check=0
-        else
-            old=$(cat "${state}")
-
-            if [ "${new}" -gt $((old + 86400)) ]; then
-                printf "%s" "${new}" >"${state}"
-                check=0
-            fi
-        fi
-
-        if [ "${check}" -eq 0 ]; then
-            _gh_dl "jtroo" "kanata" "kanata_winIOv2.exe" "browser_download_url" "${USERPROFILE:?}/kanata/kanata.exe.new"
-            _gh_dl "oblitum" "Interception" "Interception.zip" "browser_download_url" "${USERPROFILE:?}/kanata/interception.zip"
-        else
-            _quiet "Skipping kanata.exe"
-        fi
-
         # alacritty
         mkdir -p "${APPDATA:?}/alacritty"
         cp "${SRC:?}/private_dot_config/alacritty/"* "${APPDATA:?}/alacritty/"
@@ -78,12 +49,8 @@ case "${CHEZMOI_OS:?}" in
         mkdir -p "${USERPROFILE:?}/.config/wezterm"
         cp "${SRC:?}/private_dot_config/wezterm/"* "${USERPROFILE:?}/.config/wezterm/"
         ;;
-    *)
-        _quiet "Skipping, wsl not found"
-        ;;
+    *) ;;
     esac
     ;;
-*)
-    _quiet "Skipping, wsl not found"
-    ;;
+*) ;;
 esac
