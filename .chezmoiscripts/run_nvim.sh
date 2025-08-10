a2034869c3f8c5c76c9cd731c69103247d4d0bd5
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2025-08-10 16:13:03 -0400

    chezmoiexternal: Add jj, zellij, and nvim

--- .chezmoiscripts/run_nvim.sh
+++ .chezmoiscripts/run_nvim.sh
@@ -6,11 +6,5 @@
 _check "$0"
 
 if [ "${_run}" -eq 0 ]; then
-    _gh_dl "neovim" "neovim" "nvim-linux-x86_64.appimage" "browser_download_url"
-    if [ -f "${_target}" ]; then
-        chmod u+x "${_target}"
-        mv "${_target}" "${HOME}/.local/bin/nvim"
-    fi
-
     _git_list "${_src}/src/nvim/git" "${HOME}/.local/share/nvim/site/pack/bundle/start"
 fi
