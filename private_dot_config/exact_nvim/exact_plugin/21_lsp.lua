65a63480bf0e0358c3c416161e3eedcba82e179a
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2026-03-09 17:22:48 -0400

    nvim: Add more binds for rustaceanvim

--- private_dot_config/exact_nvim/exact_plugin/21_lsp.lua
+++ private_dot_config/exact_nvim/exact_plugin/21_lsp.lua
@@ -35,12 +35,17 @@ now_if_args(function()
 
     vim.g.rustaceanvim = {
         server = {
-            on_attachp = function(client, buf)
+            on_attach = function(client, buf)
                 map(n, '<Leader>la', 'Actions (Rustaceanvim)',
                     cmd('RustLsp codeAction'), { buffer = buf })
 
+                map(n, '<Leader>ld', 'Diagnostic (Rustaceanvim)',
+                    cmd('RustLsp renderDiagnostics current'), { buffer = buf })
+
                 map(n, 'K', 'Hover (Rustaceanvim)',
                     cmd('RustLsp hover actions'), { buffer = buf })
+
+                vim.lsp.inlay_hint.enable(true, { bufnr = buf })
             end,
             default_settings = {
                 ['rust-analyzer'] = {
