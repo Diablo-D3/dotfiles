9be13b67c2bc0b955b52274362cf2f22dfd14b22
Patrick McFarland <pmcfarland@adterrasperaspera.com>
2026-05-18 08:40:11 -0400

    nvim: Improve ts config, add gotmpl ts injector

--- private_dot_config/exact_nvim/exact_plugin/23_treesitter.lua
+++ private_dot_config/exact_nvim/exact_plugin/23_treesitter.lua
@@ -10,25 +10,58 @@ now_if_args(function()
         'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
     })
 
-    local treesitter = require('nvim-treesitter')
-    local avail = treesitter.get_available()
-    local ins = treesitter.get_installed()
+    _G.nvim_treesitter = require('nvim-treesitter')
 
     create_autocmd('FileType', 'nvim-treesitter setup', {
         callback = function(ev)
             local match = vim.treesitter.language.get_lang(ev.match)
 
+            local avail = nvim_treesitter.get_available()
+            local ins = nvim_treesitter.get_installed()
+
             if (tbl_contains(avail, match) and not tbl_contains(ins, match)) then
-                treesitter.install(match):wait(30000)
-                ins = treesitter.get_installed()
+                nvim_treesitter.install(match):wait(30000)
+                ins = nvim_treesitter.get_installed()
             end
 
             if (tbl_contains(ins, match)) then
-                vim.treesitter.start()
+                vim.treesitter.start(ev.buf)
                 vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
                 vim.wo[0][0].foldmethod = 'expr'
                 vim.bo.indentexpr = 'v:lua.require(\'nvim-treesitter\').indentexpr()'
             end
         end
     })
+
+    -- Originally from https://github.com/ngynkvn/gotmpl.nvim, which seems to be abandoned
+    -- Includes changes from:
+    -- ngynkvn/gotmpl.nvim#1 from eggplannt: "fixed edge cases and moved query to inline lua"
+    -- ngynkvn/gotmpl.nvim#2 from pddshk: "merge scm file into already loaded with '; extends'"
+    -- Further modifications are mine
+    vim.treesitter.query.add_directive('inject-go-tmpl!', function(_, _, bufnr, _, metadata)
+        local fname = vim.api.nvim_buf_get_name(bufnr + 0):match("(.+).tmpl$")
+        local ft = vim.filetype.match({ buf = bufnr + 0, filename = fname })
+
+        if not ft then
+            return
+        end
+
+        metadata['injection.language'] = ft
+    end, {})
+
+    -- override .tmpl to be "gotmpl" and not "template"
+    vim.filetype.add({
+        extension = {
+            tmpl = 'gotmpl',
+        },
+    })
+
+    vim.treesitter.query.set('gotmpl', 'injections', [[
+        ; extends
+        ((text) @injection.content
+            (#inject-go-tmpl!)
+            (#set! injection.combined)
+            (#set! injection.include-children))
+        ]]
+    )
 end)
